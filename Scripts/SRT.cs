b08f8eabb6248250d151f1464aa466c117013652
Scott Doxey <neogeek0101@gmail.com>
2020-01-19 08:56:14 -0500

    Added SRT class.

---
+++ Scripts/SRT.cs
@@ -0,0 +1,25 @@
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace A11YTK
+{
+
+    public static class SRT
+    {
+
+        public struct Subtitle
+        {
+
+            public int id;
+
+            public double startTime;
+
+            public double endTime;
+
+            public string text;
+
+        }
+
+    }
+
+}
