ad4cd9419da96419b27310ec8cf324626e64e72b
Scott Doxey <neogeek0101@gmail.com>
2020-01-25 17:37:25 -0500

    Added static methods for parsing SRT files.
    Added tests.

--- Scripts/SRT.cs
+++ Scripts/SRT.cs
@@ -1,5 +1,9 @@
-using System.Text.RegularExpressions;
-using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace A11YTK
 {
@@ -17,6 +21,84 @@ namespace A11YTK
             public double endTime;
 
             public string text;
+
+        }
+
+        public static readonly string[] TIMESTAMP_DELIMITER = { "-->" };
+
+        public static double ParseMillisecondsFromTimeStamp(string timestamp, out TimeSpan results)
+        {
+
+            TimeSpan.TryParse(timestamp.Trim().Replace(',', '.'), out results);
+
+            return results.TotalMilliseconds;
+
+        }
+
+        public static void ParseTimeFromContent(string content, out double startTime, out double endTime)
+        {
+
+            var times = content.Split(TIMESTAMP_DELIMITER, StringSplitOptions.None);
+
+            try
+            {
+
+                startTime = ParseMillisecondsFromTimeStamp(times[0], out _);
+                endTime = ParseMillisecondsFromTimeStamp(times[1], out _);
+
+            }
+            catch (Exception _)
+            {
+
+                startTime = -1;
+                endTime = -1;
+
+                throw new WarningException("Non-valid timestamp range.");
+
+            }
+
+        }
+
+        public static List<Subtitle> ParseSubtitlesFromFile(string path)
+        {
+
+            return ParseSubtitlesFromString(File.ReadAllText(path, Encoding.UTF8));
+
+        }
+
+        public static List<Subtitle> ParseSubtitlesFromString(string content)
+        {
+
+            var subtitles = new List<Subtitle>();
+
+            var matches = content.Split(new char[] { '\n' }).ToList().ChunkListWithPatternDelimiter(@"^\s*$");
+
+            foreach (var match in matches)
+            {
+
+                var subtitle = new Subtitle();
+
+                int.TryParse(match[0], out subtitle.id);
+
+                ParseTimeFromContent(match[1], out subtitle.startTime, out subtitle.endTime);
+
+                var text = new StringBuilder();
+
+                for (var i = 2; i < match.Count; i += 1)
+                {
+
+                    text.Append(match[i]);
+                    text.Append(Environment.NewLine);
+
+                }
+
+                subtitle.text = text.ToString().Trim();
+
+                subtitles.Add(subtitle);
+
+            }
+
+            return subtitles;
 
         }
 
