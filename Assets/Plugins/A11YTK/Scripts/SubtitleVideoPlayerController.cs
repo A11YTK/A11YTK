fe79da6115261f18a84168b1ee684f30227a574f
Scott Doxey <neogeek0101@gmail.com>
2020-01-26 09:03:54 -0500

    Added SRT support to subtitle controller.
    Added audio and video specific controllers.

---
+++ Assets/Plugins/A11YTK/Scripts/SubtitleVideoPlayerController.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace A11YTK
+{
+
+    [AddComponentMenu("A11YTK/Subtitle VideoPlayer Controller")]
+    [RequireComponent(typeof(SubtitleRenderer))]
+    public class SubtitleVideoPlayerController : SubtitleController
+    {
+
+#pragma warning disable CS0649
+        [SerializeField]
+        private VideoPlayer _videoSource;
+#pragma warning restore CS0649
+
+        protected override double elapsedTime => _videoSource.time;
+
+        public override void Play()
+        {
+
+            base.Play();
+
+            if (_videoSource)
+            {
+
+                _videoSource.Play();
+
+            }
+
+        }
+
+        public override void Stop()
+        {
+
+            base.Stop();
+
+            if (_videoSource)
+            {
+
+                _videoSource.Stop();
+
+            }
+
+        }
+
+        private void OnValidate()
+        {
+
+            if (_videoSource == null)
+            {
+
+                _videoSource = gameObject.GetComponent<VideoPlayer>();
+
+            }
+
+        }
+
+    }
+
+}
