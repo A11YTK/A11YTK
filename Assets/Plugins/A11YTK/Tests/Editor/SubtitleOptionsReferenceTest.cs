6c0c931b3b540b7a96ce4a1b7e8967182e785f9f
Scott Doxey <neogeek0101@gmail.com>
2020-02-04 23:57:09 -0500

    Added Save/Load/Delete methods to subtitle options.

---
+++ Assets/Plugins/A11YTK/Tests/Editor/SubtitleOptionsReferenceTest.cs
@@ -0,0 +1,52 @@
+using System.IO;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+
+namespace A11YTK.Tests
+{
+
+    public static class SubtitleOptionsReferenceTest
+    {
+
+        private const string subtitlesOptionsFileName = "SubtitleOptions.dat";
+
+        private static string subtitlesOptionsPath =>
+            Path.Combine(Application.persistentDataPath, subtitlesOptionsFileName);
+
+        [SetUp, TearDown]
+        public static void DeleteFiles()
+        {
+
+            FileUtil.DeleteFileOrDirectory(subtitlesOptionsPath);
+
+        }
+
+        [Test]
+        public static void SaveAndLoad()
+        {
+
+            var subtitleOptions = ScriptableObject.CreateInstance<SubtitleOptionsReference>();
+
+            subtitleOptions.enabled = true;
+            subtitleOptions.fontSize = 24;
+            subtitleOptions.fontForegroundColor = Color.yellow;
+
+            subtitleOptions.Save(subtitlesOptionsFileName);
+
+            var loadedSubtitleOptions = ScriptableObject.CreateInstance<SubtitleOptionsReference>();
+
+            loadedSubtitleOptions.Load(subtitlesOptionsFileName);
+
+            Assert.AreEqual(true, loadedSubtitleOptions.enabled);
+            Assert.AreEqual(24, loadedSubtitleOptions.fontSize);
+            Assert.IsTrue(Mathf.Approximately(Color.yellow.r, loadedSubtitleOptions.fontForegroundColor.r));
+            Assert.IsTrue(Mathf.Approximately(Color.yellow.g, loadedSubtitleOptions.fontForegroundColor.g));
+            Assert.IsTrue(Mathf.Approximately(Color.yellow.b, loadedSubtitleOptions.fontForegroundColor.b));
+            Assert.IsTrue(Mathf.Approximately(Color.yellow.a, loadedSubtitleOptions.fontForegroundColor.a));
+
+        }
+
+    }
+
+}
