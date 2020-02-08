efe6caca84959cc8695b51bfc79551b9711e9e6d
Scott Doxey <neogeek0101@gmail.com>
2020-02-08 09:27:45 -0500

    Moved methods from utilities to custom extensions file.

--- Assets/Plugins/A11YTK/Scripts/CustomExtensions.cs
+++ Assets/Plugins/A11YTK/Scripts/CustomExtensions.cs
@@ -91,6 +91,24 @@ namespace A11YTK
 
         }
 
+        public static void ResetRectTransform(this RectTransform rectTransform)
+        {
+
+            rectTransform.anchorMin = Vector2.zero;
+            rectTransform.anchorMax = Vector2.one;
+
+            rectTransform.offsetMin = Vector2.zero;
+            rectTransform.offsetMax = Vector2.zero;
+
+        }
+
+        public static void ScaleCanvasToMatchCamera(this Canvas canvas, Camera camera)
+        {
+
+            canvas.transform.localScale = Vector3.one * 0.025f;
+
+        }
+
     }
 
 }
