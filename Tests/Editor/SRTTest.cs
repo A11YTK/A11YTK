3dbacb417336f2341a778be953c5021d5718890c
Scott Doxey <neogeek0101@gmail.com>
2020-02-15 10:34:03 -0500

    Updated tests.

--- Tests/Editor/SRTTest.cs
+++ Tests/Editor/SRTTest.cs
@@ -43,18 +43,18 @@ Fine. If you can't wave, just yell out.
                 SRT.ParseSubtitlesFromString(MOCK_SUBTITLE_CONTENTS);
 
             Assert.AreEqual(1, subtitles[0].id);
-            Assert.AreEqual(4200d, subtitles[0].startTime);
-            Assert.AreEqual(8200d, subtitles[0].endTime);
+            Assert.AreEqual(4.200d, subtitles[0].startTime);
+            Assert.AreEqual(8.200d, subtitles[0].endTime);
             Assert.AreEqual("Hello, world.", subtitles[0].text);
 
             Assert.AreEqual(4, subtitles[3].id);
-            Assert.AreEqual(32200d, subtitles[3].startTime);
-            Assert.AreEqual(36200d, subtitles[3].endTime);
+            Assert.AreEqual(32.200d, subtitles[3].startTime);
+            Assert.AreEqual(36.200d, subtitles[3].endTime);
             Assert.AreEqual("If you can see me,\ncan you wave?", subtitles[3].text);
 
             Assert.AreEqual(5, subtitles[4].id);
-            Assert.AreEqual(40200d, subtitles[4].startTime);
-            Assert.AreEqual(44200d, subtitles[4].endTime);
+            Assert.AreEqual(40.200d, subtitles[4].startTime);
+            Assert.AreEqual(44.200d, subtitles[4].endTime);
             Assert.AreEqual("Fine. If you can't wave, just yell out.", subtitles[4].text);
 
         }
@@ -73,8 +73,8 @@ Fine. If you can't wave, just yell out.
 
             SRT.ParseTimeFromContent("0:0:4,200 --> 0:0:8,200", out var startTime, out var endTime);
 
-            Assert.AreEqual(4200d, startTime);
-            Assert.AreEqual(8200d, endTime);
+            Assert.AreEqual(4.200d, startTime);
+            Assert.AreEqual(8.200d, endTime);
 
         }
 
