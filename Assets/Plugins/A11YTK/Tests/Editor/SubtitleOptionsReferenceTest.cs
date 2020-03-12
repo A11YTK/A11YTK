30e3090c5e16f9c78d3b9f6f0ace9a5a05413116
Scott Doxey <neogeek0101@gmail.com>
2020-03-12 02:54:05 -0400

    Updated test.

--- Assets/Plugins/A11YTK/Tests/Editor/SubtitleOptionsReferenceTest.cs
+++ Assets/Plugins/A11YTK/Tests/Editor/SubtitleOptionsReferenceTest.cs
@@ -34,6 +34,8 @@ namespace A11YTK.Tests
             subtitleOptions.enabled = true;
             subtitleOptions.fontSize = 24;
             subtitleOptions.fontColor = Color.yellow;
+            subtitleOptions.backgroundColor = Color.black;
+            subtitleOptions.showBackground = false;
 
             subtitleOptions.Save(subtitlesOptionsFileName);
 
@@ -48,6 +50,13 @@ namespace A11YTK.Tests
             Assert.Less(Mathf.Abs(Color.yellow.g - loadedSubtitleOptions.fontColor.g), COLOR_EPSILON);
             Assert.Less(Mathf.Abs(Color.yellow.b - loadedSubtitleOptions.fontColor.b), COLOR_EPSILON);
             Assert.Less(Mathf.Abs(Color.yellow.a - loadedSubtitleOptions.fontColor.a), COLOR_EPSILON);
+
+            Assert.Less(Mathf.Abs(Color.black.r - loadedSubtitleOptions.backgroundColor.r), COLOR_EPSILON);
+            Assert.Less(Mathf.Abs(Color.black.g - loadedSubtitleOptions.backgroundColor.g), COLOR_EPSILON);
+            Assert.Less(Mathf.Abs(Color.black.b - loadedSubtitleOptions.backgroundColor.b), COLOR_EPSILON);
+            Assert.Less(Mathf.Abs(Color.black.a - loadedSubtitleOptions.backgroundColor.a), COLOR_EPSILON);
+
+            Assert.AreEqual(false, loadedSubtitleOptions.showBackground);
 
         }
 
