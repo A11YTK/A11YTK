0f855efc8e1d0ae5a63a607566d2969dd9c50c2a
Scott Doxey <neogeek0101@gmail.com>
2020-03-13 02:50:40 -0400

    Added TooltipController component.

---
+++ Assets/Plugins/A11YTK/Scripts/TooltipController.cs
@@ -0,0 +1,114 @@
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace A11YTK
+{
+
+    [AddComponentMenu("A11YTK/Tooltip Controller")]
+    public class TooltipController : MonoBehaviour
+    {
+
+#pragma warning disable CS0649
+        [SerializeField]
+        [TextArea(1, 10)]
+        protected string _tooltipText = "Hello, world.";
+
+        [SerializeField]
+        protected Subtitle.Position _position = Subtitle.Position.AUTO;
+
+        [SerializeField]
+        protected Subtitle.Mode _mode = Subtitle.Mode.AUTO;
+
+        [SerializeField]
+        protected SubtitleOptionsReference _subtitleOptions;
+#pragma warning restore CS0649
+
+        public Subtitle.Position position =>
+            _position.Equals(Subtitle.Position.AUTO) ? _subtitleOptions.defaultPosition : _position;
+
+        public Subtitle.Mode mode =>
+            _mode.Equals(Subtitle.Mode.AUTO) ? _subtitleOptions.defaultMode : _mode;
+
+        public SubtitleOptionsReference subtitleOptions => _subtitleOptions;
+
+        protected SubtitleRenderer _subtitleRenderer;
+
+        private void Awake()
+        {
+
+            if (_subtitleOptions == null)
+            {
+
+                Debug.LogError("Subtitle options asset is missing!");
+
+            }
+
+            if (subtitleOptions.defaultMode.Equals(Subtitle.Mode.SCREEN) && UnityEngine.XR.XRSettings.enabled)
+            {
+
+                Debug.LogWarning("Subtitles will not render in SCREEN mode while running in VR!");
+
+                subtitleOptions.defaultMode = Subtitle.Mode.HEADSET;
+
+            }
+
+            var subtitlePrefab =
+                Resources.LoadAll<GameObject>("Prefabs")
+                    .First(material =>
+                        material.name.StartsWith("Subtitle") && material.name.Contains(mode.ToString()));
+
+            _subtitleRenderer = Instantiate(subtitlePrefab).GetComponent<SubtitleRenderer>();
+
+            _subtitleRenderer.mode = mode;
+            _subtitleRenderer.position = position;
+            _subtitleRenderer.targetTransform = gameObject.transform;
+            _subtitleRenderer.targetCollider = gameObject.GetComponent<Collider>();
+
+            _subtitleRenderer.SetOptions(_subtitleOptions);
+
+        }
+
+        private void Start()
+        {
+
+            _subtitleRenderer.Show();
+
+            _subtitleRenderer.SetText(_tooltipText);
+
+        }
+
+        private void OnEnable()
+        {
+
+            _subtitleRenderer.Show();
+
+        }
+
+        private void OnDisable()
+        {
+
+            _subtitleRenderer.Hide();
+
+        }
+
+#if UNITY_EDITOR
+        protected virtual void OnValidate()
+        {
+
+            if (_subtitleOptions == null)
+            {
+
+                _subtitleOptions =
+                    AssetDatabase.LoadAssetAtPath<SubtitleOptionsReference>(AssetDatabase.GUIDToAssetPath(AssetDatabase
+                        .FindAssets("t:SubtitleOptionsReference", null)
+                        .FirstOrDefault()));
+
+            }
+
+        }
+#endif
+
+    }
+
+}
